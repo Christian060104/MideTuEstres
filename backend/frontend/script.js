a4f75d8a45a7ea494aff7f7c85adef122905e584
Christian cc <chriscc@gmail.com>
2026-06-09 00:00:41 -0600

    Poner el mod oscuro

--- backend/frontend/script.js
+++ backend/frontend/script.js
@@ -1010,3 +1010,30 @@ document.addEventListener("click", (e) => {
     generatePDF();
   }
 });
+
+// ==========================
+// MODO OSCURO / CLARO
+// ==========================
+
+const themeToggle = document.getElementById("themeToggle");
+
+// Cargar tema guardado
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "dark") {
+  document.body.classList.add("dark-mode");
+  themeToggle.textContent = "☀️";
+}
+
+// Cambiar tema
+themeToggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+
+  if (document.body.classList.contains("dark-mode")) {
+    localStorage.setItem("theme", "dark");
+    themeToggle.textContent = "☀️";
+  } else {
+    localStorage.setItem("theme", "light");
+    themeToggle.textContent = "🌙";
+  }
+});
